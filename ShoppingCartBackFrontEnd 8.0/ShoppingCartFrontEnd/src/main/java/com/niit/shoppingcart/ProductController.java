a7e3ee3834bafe6ea4ef2e108a8ca9f34ab2b6e5
Roopesh <unnamatlaroopesh@gmail.com>
2016-09-27 21:25:52 +0530

    with multipart

---
+++ ShoppingCartBackFrontEnd 8.0/ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/ProductController.java
@@ -0,0 +1,118 @@
+package com.niit.shoppingcart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.dao.ProductDAO;
+import com.niit.shoppingcart.dao.SupplierDAO;
+import com.niit.shoppingcart.model.Category;
+import com.niit.shoppingcart.model.Product;
+import com.niit.shoppingcart.model.Supplier;
+import com.niit.shoppingcart.util.FileUtil;
+
+
+
+
+
+@Controller
+public class ProductController {
+	@Autowired(required=true)
+	 private ProductDAO productDAO;
+	
+	@Autowired
+	Category category;
+	@Autowired
+	Supplier supplier;
+	@Autowired
+	Product product;
+	@Autowired(required=true)
+	private CategoryDAO categoryDAO;
+	@Autowired(required=true)
+	private SupplierDAO supplierDAO;
+	private String Path="D:\\images";
+	@RequestMapping(value="/product",method=RequestMethod.GET)
+	public String listProducts(Model model){
+		model.addAttribute("product", product);
+		model.addAttribute("category", category);
+		model.addAttribute("supplier", supplier);
+		model.addAttribute("productList",this.productDAO.list());
+		model.addAttribute("categoryList",this.categoryDAO.list());
+		model.addAttribute("supplierList",this.supplierDAO.list());
+		return "Product";
+
+	}
+	@RequestMapping(value="product/add",method=RequestMethod.POST)
+	public String addProduct(@ModelAttribute("product") Product product,@RequestParam("image")MultipartFile file){
+
+              if (productDAO.get(product.getPro_id()) == null) {
+       			productDAO.save(product);
+       		} else {
+       			
+       				productDAO.update(product);
+       		}
+                 FileUtil.upload(Path, file, product.getPro_id()+".jpg");           
+                  return "redirect:/manageProducts";
+	}
+	 	
+	@RequestMapping("product/remove/{pro_id}")
+	public String removeProduct(@PathVariable("pro_id") String id)
+	{
+		System.out.println("in delete method");
+		Product product=productDAO.get(id);
+		ModelAndView mv = new ModelAndView("category");
+	
+	boolean flag=	productDAO.delete(product);
+		if(flag==true){
+			System.out.println("i am in delete operation");
+		}
+		else{
+			mv.addObject("ErrorMessage", "could not delete the record");
+		}
+		return "redirect:/manageProducts";
+		
+	}
+	@RequestMapping("product/edit/{pro_id}")
+	public String removeProduct(@PathVariable("pro_id") String id,Model model){
+		System.out.println(id);
+		product=productDAO.get(id);
+		ModelAndView mv = new ModelAndView();
+
+		if (productDAO.get(product.getPro_id()) != null) {
+			productDAO.update(product);
+			System.out.println("i am in update");
+			//log.debug("Ending  of the method update");
+		} else {
+			mv.addObject("ErrorMessage", "couldnot update the record");
+		}
+		return "redirect:/product";
+
+	}
+	/*@RequestMapping("product/get/{pro_id}")
+	public String getSelectedProduct(@PathVariable("pro_id") String id,Model model,
+		  RedirectAttributes redirectAttributes){
+		   redirectAttributes.addFlashAttribute("selectedProduct", productDAO.get(id));
+			return "Product";
+	}*/
+	
+/*	@RequestMapping(value="/backtoHome",method=RequestMethod.GET)
+	  Public String backtoHome(@ModelAttribute("selectedProduct")
+	      final Object selectedProduct,final Model model)
+	  {
+		model.addAttribute("selectedProduct", selectedProduct)
+		return "/home";
+	  }
+	*/
+
+}
---
+++ ShoppingCartBackFrontEnd 8.0/ShoppingCartFrontEnd/src/main/webapp/views/Product.jsp
@@ -0,0 +1,117 @@
+ <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<link rel="stylesheet"href="<c:url value="/resources/admin.css"/>">
+<html>
+<head>
+<title>Product Page</title>
+</head>
+<body>
+${msg}
+<h1>Add product</h1>
+<c:url var="addAction" value="product/add"></c:url>
+<form:form action="${addAction}" commandName="product" enctype="multipart/form.data">
+<table>
+	<tr>
+			<td><form:label path="pro_id"> <spring:message text="ID"/></form:label></td>
+					<c:choose>
+							<c:when test="${!empty product.pro_id}">
+							<td><form:input path="pro_id" readonly="true"/> </td>
+							</c:when>
+									
+							 <c:otherwise>
+							 <td><form:input path="pro_id" pattern="{5,10}" required="true" title="id should be between 5 to 10 characters"/> </td>
+							 </c:otherwise>
+					</c:choose>
+			</tr>
+		<tr>
+			<td><form:label path="pro_name"> <spring:message text="NAME"/></form:label></td>
+			<td><form:input path="pro_name" required="true"/> </td>
+		</tr>
+		
+		<tr>
+			<td><form:label path="pro_description"> <spring:message text="DESCRIPTION"/></form:label></td>
+			<td><form:input path="pro_description" required="true"/> </td>
+		</tr>
+		
+		<tr>
+			<td><form:label path="pro_price"> <spring:message text="PRICE"/></form:label></td>
+			<td><form:input path="pro_price" required="true"/> </td>
+		</tr>
+							
+	       <tr>
+	  <td><form:label path="supplier"><spring:message text="Supplier"/></form:label></td>
+	  
+	  <td><form:select path="supplier_id">
+	           <c:forEach items="${supplierList}" var="supplier">
+								<form:option class="input1" value="${supplier.sup_id}">${supplier.sup_name}</form:option>
+							</c:forEach>
+							</form:select>
+							</td>
+	           </tr>
+	           
+	       <tr>
+	  <td><form:label path="category"><spring:message text="Category"/></form:label></td>
+	  <td><form:select path="category_id"  >
+	           <c:forEach items="${categoryList}" var="category">
+								<form:option class="input1" value="${category.cat_id}">${category.cat_name}</form:option>
+							</c:forEach>
+							</form:select>
+							</td>
+	           </tr> 
+	           <tr>
+					<td>Image:</td>
+					<td><form:input type="file"
+							class=" btn btn-default btn-block form-control" path="image"
+							required="true" /></td>
+				</tr>
+		<tr>
+			<td colspan="2">
+			<c:if test="${!empty product.pro_name}">
+			<input type="submit" value="<spring:message text="Edit product"/> "> 
+			</c:if>
+			<c:if test="${empty product.pro_name}">
+			<input type="submit" value="<spring:message text="Add product"/> "> 
+			</c:if>
+			</td>
+
+		</tr>
+</table>
+</form:form>
+<br>
+
+	              
+	             
+	            <h3>Product List</h3>
+<c:if test="${!empty productList}">
+	<table class="tg">
+	<tr>
+		<th width="80"> Product Id</th>
+		<th width="80">Product Name</th>
+		<th width="100">Product Description</th>
+				<th width="80">Product Price</th>
+					<th width="80">Category</th>
+						<th width="80">Supplier</th>
+						<th width="120">Image</th>
+		<th>Edit</th>
+		<th>Delete</th>
+	</tr>
+	<c:forEach items="${productList}" var="product">
+		<tr>
+			<td>${product.pro_id}</td>
+			<td>${product.pro_name}</td>
+			<td>${product.pro_description}</td>
+			<td>${product.pro_price}</td>
+			<td>${product.category_id}<td>
+			<td>${product.supplier_id} </td>
+			<td><div class="thumbnail">
+								<img height="100px" width="100px" alt="${product.pro_id }"
+									src="<c:url value="D:\images/${product.pro_id }.jpg"></c:url>">
+			<td><a href="<c:url value="product/edit/${product.pro_id}"/>" >Edit</a></td>
+			<td><a href="<c:url value="product/remove/${product.pro_id}"/>" >Delete</a></td>
+		</tr>
+	</c:forEach>
+	</table>
+</c:if>
+</body>
+</html> 
