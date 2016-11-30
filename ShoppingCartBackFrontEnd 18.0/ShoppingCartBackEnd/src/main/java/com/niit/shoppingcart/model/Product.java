f5b15772cac720fe1f31626f21cdad8e742e2760
Roopesh <unnamatlaroopesh@gmail.com>
2016-11-30 21:56:24 +0530

    with spring security role_user

---
+++ ShoppingCartBackFrontEnd 18.0/ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/model/Product.java
@@ -0,0 +1,99 @@
+package com.niit.shoppingcart.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table
+@Component
+public class Product {
+
+	@Id
+	private String pro_id;
+	private String pro_name;
+	private String pro_description;
+	private int pro_price;
+
+
+	private String category_id;
+		private String supplier_id;
+		@Transient
+		private MultipartFile image;
+		
+		public MultipartFile getImage() {
+			return image;
+		}
+		public void setImage(MultipartFile image) {
+			this.image = image;
+		}
+		public String getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(String category_id) {
+		this.category_id = category_id;
+	}
+		
+
+	public String getSupplier_id() {
+			return supplier_id;
+		}
+		public void setSupplier_id(String supplier_id) {
+			this.supplier_id = supplier_id;
+		}
+	
+	@ManyToOne
+	@JoinColumn(name="category_id" ,updatable=false,insertable=false,nullable=false)
+	private Category category;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="supplier_id", updatable=false,insertable=false,nullable=false)
+	private Supplier supplier;
+	
+		 public Supplier getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	
+
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getPro_id() {
+		return pro_id;
+	}
+	public void setPro_id(String pro_id) {
+		this.pro_id = pro_id;
+	}
+	public String getPro_name() {
+		return pro_name;
+	}
+	public void setPro_name(String pro_name) {
+		this.pro_name = pro_name;
+	}
+	public String getPro_description() {
+		return pro_description;
+	}
+	public void setPro_description(String pro_description) {
+		this.pro_description = pro_description;
+	}
+	
+	public int getPro_price() {
+		return pro_price;
+	}
+	public void setPro_price(int pro_price) {
+		this.pro_price = pro_price;
+	}
+
+}
---
+++ ShoppingCartBackFrontEnd 18.0/ShoppingCartFrontEnd/src/main/webapp/WEB-INF/views/viewProduct.jsp
@@ -0,0 +1,54 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<html>
+<head>
+<style>
+ul li{
+
+    padding: 5px;
+}
+
+.jumbotron {
+padding-top: 60px;
+    padding-bottom: 230px;
+    margin-top: 50px;
+}
+}
+
+</style>
+</head>
+<body>
+<div class="container">
+				<div class="jumbotron">
+					  <div class="col-md-12">
+			                                <div class="col-md-6">
+               <img src="<c:url value="E:\\workspace\\ShoppingCartFrontEnd\\src\\main\\resources\\images\\${product.pro_id}.jpg"/>" alt="image" style="width:50%">   
+                  
+              
+        							</div>
+                <div class="col-md-6">
+			<ul class="list-unstyled">
+			
+					<li ><em>Product: ${product.pro_name}</em></li>
+					<li > <em>Description: ${product.pro_description}</em></li>
+					<li><em>Price: Rs. ${product.pro_price}</em></li>
+					<li ><em>Category: ${product.category.cat_name}</em></li>
+					<li > <em>Supplier: ${product.supplier.sup_name}</em></li>
+     <li><c:url var="action" value="/addtocart/${product.pro_id}"></c:url>
+			<form:form action="${action}" >
+			<input type="submit" class="btn btn-primary" value="Add To Cart" /></form:form></li> 
+	
+			
+        	</ul> 
+</div>
+	    </div> 
+
+				
+				</div>
+
+
+			</div>
+
+</body>
+</html>
